da835f9ce15184ec7cab065d0dc47e67c85aaebb
Yen Kuo <kuoyen5@gmail.com>
2024-07-31 21:51:28 +0930

    adding detail page as well

--- Controllers/ClubController.cs
+++ Controllers/ClubController.cs
@@ -1,18 +1,40 @@
-using System;
-using System.Collections.Generic;
+
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using MVCPractice.data.Enum;
+using MVCPractice.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MVCPractice.Controllers
 {
     public class ClubController : Controller
     {
+        private readonly ILogger<ClubController> _logger;
+
+        private readonly ApplicationDbContext _context;
+        public ClubController(ILogger<ClubController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var club = _context.Clubs.Include(a => a.Address).First(club => club.Id == id);
+            return View(club);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- Controllers/RaceController.cs
+++ Controllers/RaceController.cs
@@ -1,14 +1,43 @@
 
+using System.Data.Common;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using MVCPractice.data.Enum;
+using MVCPractice.Models;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace MVCPractice.Controllers
 {
     public class RaceController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+
+        private ILogger<RaceController> _logger;
+
+        public RaceController(ILogger<RaceController> logger, ApplicationDbContext context)
+        {
+            _context = context;
+            _logger = logger;
+        }
         public IActionResult Index()
         {
-            return View();
+
+            var races = _context.Races.ToList();
+            return View(races);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var race = _context.Races.Include(a => a.Address).First(r => r.Id == id);
+            return View(race);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
